66953e4126eb72dbf15f290be831077f2b36dd76
BatuAlk <bthn.efealkis@gmail.com>
2023-07-17 11:32:00 +0300

    fixed editing, drag and drop

--- js/template.js
+++ js/template.js
@@ -12,7 +12,6 @@ const addTask = () => {
 	} else {
 		const newTask = document.createElement('li');
 		newTask.draggable = true;
-		newTask.id = 'todo-task' + todoList.children.length;
 		newTask.addEventListener('dragstart', dragStart, false);
 		newTask.innerHTML = `<span class="task-name">${taskName}</span><button class="edit" onclick="editTask('.todo-items')">Edit</button><button class="remove" onclick="removeTask('.todo-items')">Remove</button>`;
 		todoList.appendChild(newTask);
@@ -57,7 +56,7 @@ const editTask = (itemList) => {
 				li.removeChild(edit);
 				li.removeChild(remove);
 				input.focus();
-
+				
 				const saveButton = document.createElement('button');
 				saveButton.className = 'save';
 				saveButton.innerHTML = 'Save';
@@ -68,6 +67,18 @@ const editTask = (itemList) => {
 				editButtons.forEach((button) => {
 					button.disabled = true;
 					button.style.background = 'gray';
+					button.style.cursor = 'not-allowed';
+				});
+				const removeButtons = document.querySelectorAll('.remove');
+				removeButtons.forEach((button) => {
+					button.disabled = true;
+					button.style.background = 'gray';
+					button.style.cursor = 'not-allowed';
+				});
+				const draggables = document.querySelectorAll('li');
+				draggables.forEach((draggable) => {
+					draggable.style.cursor = 'default';
+					draggable.draggable = false;
 				});
 
 				li.appendChild(saveButton);
@@ -96,6 +107,16 @@ const editTask = (itemList) => {
 						editButtons.forEach((button) => {
 							button.disabled = false;
 							button.style.background = 'goldenrod';
+							button.style.cursor = 'pointer';
+						});
+						removeButtons.forEach((button) => {
+							button.disabled = false;
+							button.style.background = 'rgba(163, 0, 0, 0.862)';
+							button.style.cursor = 'pointer';
+						});
+						draggables.forEach((draggable) => {
+							draggable.style.cursor = 'grab';
+							draggable.draggable = true;
 						});
 						edited = true;
 						storeTasks();
@@ -113,6 +134,16 @@ const editTask = (itemList) => {
 					editButtons.forEach((button) => {
 						button.disabled = false;
 						button.style.background = 'goldenrod';
+						button.style.cursor = 'pointer';
+					});
+					removeButtons.forEach((button) => {
+						button.disabled = false;
+						button.style.background = 'rgba(163, 0, 0, 0.862)';
+						button.style.cursor = 'pointer';
+					});
+					draggables.forEach((draggable) => {
+						draggable.style.cursor = 'grab';
+						draggable.draggable = true;
 					});
 					edited = true;
 				});
@@ -123,6 +154,7 @@ const editTask = (itemList) => {
 }
 
 const dragStart = (e) => {
+	e.target.style.cursor = 'grabbing';
 	const value = e.target.children[0].innerHTML;	
 	const itemList = e.target.children[0].parentElement.parentElement.className;
 	const data = {
@@ -154,49 +186,60 @@ const findIndex = (taskName, taskList) => {
 	return -1;
 }
 
+const findTarget = (target) => {
+	if (target.className === 'todo' || target.className === 'progress' || target.className === 'done') {
+		return target.children[2];
+	} else if (target.className === 'todo-items' || target.className === 'progress-items' || target.className === 'done-items') {
+		return target;
+	} else if (target.parentElement.className === 'todo-items' || target.parentElement.className === 'progress-items' || target.parentElement.className === 'done-items') {
+		return target.parentElement;
+	}
+};
+
 const drop = (e) => {
 	e.preventDefault();
 	const data = JSON.parse(e.dataTransfer.getData('data'));
 	const target = e.target;
-	if (containsTask(data["value"], target.getElementsByTagName('span'))) {
+	const validTarget = findTarget(target);
+	if (validTarget === null || validTarget === undefined) {
+		document.getElementById("error").play();
+		target.style.cursor = 'grab';
+		alert("Drop target not found!");
+		return;
+	}
+
+	if (containsTask(data["value"], validTarget.getElementsByTagName('span'))) {
 		document.getElementById("error").play();
 		alert("Task already exists!");
 		return;
 	} else {
-		if (target.className === 'todo-items') {
+		if (validTarget.className === 'todo-items') {
 			const newTask = document.createElement('li');
 			newTask.draggable = true;
-			newTask.id = 'todo-task' + target.children.length;
 			newTask.addEventListener('dragstart', dragStart, false);
 			newTask.innerHTML = `<span class="task-name">${data["value"]}</span><button class="edit" onclick="editTask('.todo-items')">Edit</button><button class="remove" onclick="removeTask('.todo-items')">Remove</button>`;
-			target.appendChild(newTask);
+			validTarget.appendChild(newTask);
 			const oldList = document.querySelector(`.${data["itemList"]}`);
 			oldList.removeChild(oldList.children[findIndex(data["value"], oldList.children)]);	
 
-		} else if (target.className === 'progress-items') {
+		} else if (validTarget.className === 'progress-items') {
 			const newTask = document.createElement('li');
 			newTask.draggable = true;
-			newTask.id = 'prog-task' + target.children.length;
 			newTask.addEventListener('dragstart', dragStart, false);
 			newTask.innerHTML = `<span class="task-name">${data["value"]}</span><button class="edit" onclick="editTask('.progress-items')">Edit</button><button class="remove" onclick="removeTask('.progress-items')">Remove</button>`;
-			target.appendChild(newTask);
+			validTarget.appendChild(newTask);
 			const oldList = document.querySelector(`.${data["itemList"]}`);
 			oldList.removeChild(oldList.children[findIndex(data["value"], oldList.children)]);
 
-		} else if (target.className === 'done-items') {
+		} else if (validTarget.className === 'done-items') {
 			const newTask = document.createElement('li');
 			newTask.draggable = true;
-			newTask.id = 'done-task' + target.children.length;
 			newTask.addEventListener('dragstart', dragStart, false);
 			newTask.innerHTML = `<span class="task-name complete">${data["value"]}</span><button class="edit" onclick="editTask('.done-items')">Edit</button><button class="remove" onclick="removeTask('.done-items')">Remove</button>`;
-			target.appendChild(newTask);
+			validTarget.appendChild(newTask);
 			const oldList = document.querySelector(`.${data["itemList"]}`);
 			oldList.removeChild(oldList.children[findIndex(data["value"], oldList.children)]);
 
-		} else {
-			document.getElementById("error").play();
-			alert("Drop target not found!");
-			return;
 		}
 	}
 	storeTasks();
@@ -220,7 +263,6 @@ window.onload = () => {
   	const addTaskToList = (taskName, list) => {
   	  	const newTask = document.createElement('li');
   	  	newTask.draggable = true;
-  	  	newTask.id = `${list}-task${document.querySelector(`.${list}-items`).children.length}`;
   	  	newTask.addEventListener('dragstart', dragStart, false);
 			if (list === 'done') {
 				newTask.innerHTML = `<span class="task-name complete">${taskName}</span><button class="edit" onclick="editTask('.${list}-items')">Edit</button><button class="remove" onclick="removeTask('.${list}-items')">Remove</button>`;
